4f1cb966628ca30e6b09337961a829e6fc2e4b05
Lucas <cekanlucas@gmail.com>
2020-04-05 01:53:13 +0000

    security for admin with HTTP basic auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name:ENV['HTTP_BASIC_AUTHENTICATE_USERNAME'], password:ENV['HTTP_BASIC_AUTHENTICATE_PASSWORD']
+
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name:ENV['HTTP_BASIC_AUTHENTICATE_USERNAME'], password:ENV['HTTP_BASIC_AUTHENTICATE_PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all
