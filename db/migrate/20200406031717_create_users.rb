026a2079330e08b6b724c28f563ed2d2aae129f4
Lucas <cekanlucas@gmail.com>
2020-04-06 03:22:40 +0000

    create model User

--- db/migrate/20200406031717_create_users.rb
+++ db/migrate/20200406031717_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :email
-      # t.string :password
 
       t.timestamps null: false
     end
