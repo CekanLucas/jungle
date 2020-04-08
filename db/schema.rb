012a218d08a35816c55161b7912790ee6e96491d
Lucas <cekanlucas@gmail.com>
2020-04-08 00:56:07 +0000

    installed bcrypt, made password field for users and display on registration page, migration successful

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200406031717) do
+ActiveRecord::Schema.define(version: 20200408004437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,8 +60,9 @@ ActiveRecord::Schema.define(version: 20200406031717) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_foreign_key "line_items", "orders"
